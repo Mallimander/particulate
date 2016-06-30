1255d8fb7cc193fb3940a63062925ed57a71ff71
Mallimander <mallimander@gmail.com>
2016-06-30 14:51:49 -0400

    Origin

---
+++ android/src/com/mallimander/particulate/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mallimander.particulate;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mallimander.particulate.Particulate;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Particulate(), config);
+	}
+}
